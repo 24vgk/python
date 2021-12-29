b2d909c4640788f076e1fab428d90ca9cf51403d
andrejkalabin <admin@24vgk.ru>
2021-12-29 20:47:08 +0300

    выполнил 2 задание

---
+++ Andrey_Kalabin_DZ/Andrey_Kalabin_lesson2/task2.py
@@ -0,0 +1,27 @@
+
+
+def search_sing(x):
+    if x[0] in '+-':
+        return x[0]
+
+
+def convert_list_in_str(list_in: list) -> str:
+    i = 0
+    while i < len(list_in):
+        changes = search_sing(list_in[i])
+        if list_in[i].isdigit() or (changes and list_in[i][1:].isdigit()):
+            if changes:
+                list_in[i] = changes + list_in[i][1:].zfill(2)
+            else:
+                list_in[i] = list_in[i].zfill(2)
+            list_in.insert(i, '"')
+            list_in.insert(i + 2, '"')
+            i += 2
+        i += 1
+    str_out = ' '.join(list_in)
+    return str_out
+
+
+my_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+result = convert_list_in_str(my_list)
+print(result)
