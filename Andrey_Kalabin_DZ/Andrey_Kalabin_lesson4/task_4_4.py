59e15e1f7c75bb6014307112a4ac95272e5b3e44
andrejkalabin <admin@24vgk.ru>
2022-01-13 21:25:26 +0300

    выполнил 4 задание

---
+++ Andrey_Kalabin_DZ/Andrey_Kalabin_lesson4/task_4_4.py
@@ -0,0 +1,13 @@
+import utils
+
+
+print(utils.currency_rates('USD'))
+print(utils.currency_rates('EUR'))
+
+"""
+/usr/local/bin/python3.9 /Users/andrejkalabin/python/11/Andrey_Kalabin_DZ/Andrey_Kalabin_lesson4/task_4_4.py
+74.5686
+85.4556
+
+Process finished with exit code 0
+"""
