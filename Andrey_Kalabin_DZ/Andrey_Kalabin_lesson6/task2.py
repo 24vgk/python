16b5bb8397837c47de6f429e434c2e5290c1dacd
andrejkalabin <admin@24vgk.ru>
2022-01-24 11:04:39 +0300

    выполнил 2 задание

---
+++ Andrey_Kalabin_DZ/Andrey_Kalabin_lesson6/task2.py
@@ -0,0 +1,31 @@
+from pprint import pprint
+
+
+def get_parse_attrs(line: str) -> tuple:
+    """Парсит строку на атрибуты и возвращает кортеж атрибутов (<remote_addr>, <request_type>, <requested_resource>)"""
+    x = line.split()
+    result = (x[0], x[5][1:], x[6])
+    return result
+
+
+def ip_spam(ip: str) -> str:
+    """ Ищет спамера по IP"""
+    x = []
+    for i in ip:
+        x.append(i[0])
+    result_spam = max(x)
+    return result_spam
+
+
+list_out = list()
+with open('nginx_logs.txt', 'r', encoding='utf-8') as fr:
+    while True:
+        row = fr.readline()
+        if not row:
+            break
+        list_out.append(get_parse_attrs(row))
+
+pprint(list_out)
+print(f'{ip_spam(list_out)} спамер')
+spam_ip = max(list_out)
+print(f'{spam_ip[0]} спамер')
